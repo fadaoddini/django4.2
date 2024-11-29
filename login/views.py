6159ad88f0ac09d32c94647f0aebc4e61e486f35
rebo <rebo@rebos-MacBook-Air.local>
2024-11-29 11:27:27 +0530

    fix bug show my product expired time4

--- login/views.py
+++ login/views.py
@@ -189,8 +189,8 @@ class GetInfo(APIView):
         try:
             user = request.user
             serializer = MyUserSerializer(user)
-            pending_count = Product.objects.filter(user=user, status=Product.PENDING).count()
-            approved_count = Product.objects.filter(user=user, status=Product.APPROVED, expire_time__gte=timezone.now()).count()
+            pending_count = Product.objects.filter(user=user, is_active=False, status=Product.PENDING).count()
+            approved_count = Product.objects.filter(user=user, is_active=True,  status=Product.APPROVED, expire_time__gte=timezone.now()).count()
             rejected_count = Product.objects.filter(user=user, status=Product.REJECTED).count()
             expired_count = Product.objects.filter(user=user, expire_time__lte=timezone.now()).count()
 
